bb32800abf6246194f91bde87748790ddd17685f
Jmik Thang <jmikthang5@gmail.com>
2026-01-25 06:57:07 -0500

    rever string which given as an array of character

---
+++ script.js
@@ -0,0 +1,21 @@
+ 
+ // Reverse String
+ // Write a function that reverses a string.
+ //  The input string is given as an array of character
+ // You must do this by modifying the input array
+let s =['w','e','l','c','o','m','e']
+function reverseString(s) {
+  let len = s.length; 
+  let halfLen = Math.floor(len / 2);
+
+  for (let i = 0; i < halfLen; i++){
+    let temp = s[i];
+     console.log(s[i])
+    s[i] = s[len - 1 - i];
+   
+    s[len - 1 - i] = temp;
+  }
+  return s;
+}
+const reverse = reverseString(s);
+console.log(reverse)
