66286792bdacedd112ef12452db69cb50075334b
Jmik Thang <jmikthang5@gmail.com>
2026-01-28 07:06:12 -0500

    intially get understand the problem but needed first in plain js, to be continue...

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -2,4 +2,15 @@
 //Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 //The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, //and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
+
+let nums1 = [1,3,5,0,0,0]; 
+let nums2 = [ 2,4,6]; 
+let m = 3; 
+let n = nums1.length; 
+
+ function mergeSortedArray() { 
+   let n = nums2.length; 
+       let i = m-1; 
+       let j = n-2; 
+       let k = (m+n) -1 
+ }
