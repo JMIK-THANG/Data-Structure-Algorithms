abc643513c967ab8c8cd82851c3426527adb4fd4
Jmik Thang <jmikthang5@gmail.com>
2026-01-30 07:13:05 -0500

    dryn run movexeroes loops

--- moveZeros.js
+++ moveZeros.js
@@ -4,12 +4,18 @@
 let nums = [0, 1, 0, 3, 12];
 
 function moveZeroes(nums) {
-  let x = 0;
+  let x = 0; // count
 
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[x] = nums[i];
+      nums[x] = nums[i]; 
+      //1  0 !== 0 ? false ? x = 0 do nothing! 
+      //2  1 !== 0 ? true,// nums[x] == 0 , nums[i] == 1  x = 1 
+      //3  0 ! == 0 ? false , x = 1 do nothing! 
+      //4  3 ! == 0 ? true , nums[x] = 1, num[i] = 3  x = 2
+      //5  12 ! == 0 ? true,  nums[x] = 2, num[i]=4 x = 3,
+       // nums = [1,3,12,3,12]; 
       x++;
     }
   }
