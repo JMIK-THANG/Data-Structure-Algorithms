52984bed2e3243df22bae6e379172cc034712f46
Jmik Thang <jmikthang5@gmail.com>
2026-02-22 20:00:06 -0500

    refactor formatting and spacing in evenOdd function for improved readability

--- evenAndOdd.js
+++ evenAndOdd.js
@@ -1,22 +1,19 @@
 // Separate even and odd numbers
 
-const arr = [1,2,3,4,5,6];
+const arr = [1, 2, 3, 4, 5, 6];
 
-const result = {even: [], odd:[]}; 
+const result = { even: [], odd: [] };
 
-function evenOdd(){
-
-      for (let i = 0; i< arr.length; i++){
-      
-            if(i%2 === 0 ){ 
-                  result.even.push(i); 
-            }
-            else { 
-                  result.odd.push(i);
-            }
-      }
-      return result; 
+function evenOdd() {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      result.even.push(i);
+    } else {
+      result.odd.push(i);
+    }
+  }
+  return result;
 }
 
-const output = evenOdd(); 
-console.log(output)
+const output = evenOdd();
+console.log(output);
